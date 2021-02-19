047ae5d0a4b252a2e703200f1ee1a61f5c7ab2f4
mpeterpeter <peter-miller@gmx.de>
2021-02-19 21:52:36 +0100

    19.2

--- myApp.js
+++ myApp.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+let bodyParser = require("body-parser");
 
 let express = require("express");
 let app = express();
@@ -11,19 +12,73 @@ app.listen(PORT, err =>{
    console.log("Listening to Port: ", PORT);
 });
 
-app.use(express.static(__dirname));
+//Body-parser for post request, besiedelt req.body mit request daten
+app.use(bodyParser.urlencoded({exended : false}));
+app.use(bodyParser.json());
 
+//ein einfacher logger: Riehenfolge spielt dabei eine Rolle
+app.use("/",(req,res,next)=>{
+   console.log(req.body);
+   console.log(`${req.method} ${req.path} - ${req.ip}`);
+   next();
+});
+
+//echo the paths /:word will match :word and place it in req.params = {word: string}
+app.get("/:word/echo", (req, res) => {
+   const { word } = req.params;
+   res.json({
+     echo: word
+   });
+ });
+
+//http query url/name?param=peter&param2=miller
+//post für die From mit action="/name"
+app.route("/name")
+   .get((req,res)=>{
+      console.log(req.query);
+      let fname = req.query.fname;
+      let lname = req.query.lname;
+      if(fname && lname){
+         res.json({name : fname + " " + lname});
+      }
+      else{
+         res.send("ParameterNames do not match!");
+      }
+   })
+   .post((req,res)=>{
+      console.log(req.body);
+      res.send(req.body.first + " " + req.body.last);
+   });
+
+//chain middleware and handler
+app.get("/now",(req,res,next)=>{
+   req.time = new Date().toString();
+   next();
+}, (req, res)=>{
+   res.json({time : req.time});
+});
+
+//serve index.html
 app.get("/",(req,res) => {
    const path = __dirname + "/views/index.html";
    res.sendFile(path);
 });
 
-app.get("/json",(req,res)=>{
+//serve style.css and the .js files (static serving)
+app.use(express.static(__dirname));
+
+app.get("/json",(req,res,next)=>{
+   let message_object = {
+      message : "hallo welt"
+   };
+
    if(process.env.MESSAGE_MODE === "uppercase"){
-      res.json("hallo welt".toUpperCase());
-      process.env.MESSAGE_MODE = "lowercase";
+      message_object.message = message_object.message.toUpperCase();
    }
-   res.json({Apfel : "300"});
+   res.json(message_object);
+   next();
 });
+
+
 
 
